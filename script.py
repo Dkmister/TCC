cfd12ecdb53bf51b137c7fa1d8df3f43060193d6
Vilmar Neto <vilmar.neto@gmail.com>
2020-11-17 01:39:18 -0300

    script com calculo de tempo atraso

---
+++ script.py
@@ -0,0 +1,22 @@
+import csv
+import pandas as pd
+
+df = pd.read_csv('Quest.csv')
+
+series = []
+
+for i in range(len(df['Semestre de Ingresso'])):
+	if df['Etapa do Ordenamento '].iloc[i] != 9 and df['Semestre de Ingresso'].iloc[i] == 1:
+		x = (2020 - df['Ano de Ingresso do Curso'].iloc[i]) * 2 - (df['Etapa do Ordenamento '].iloc[i])
+		print(x)
+		series.append(int(x))
+	elif df['Etapa do Ordenamento '].iloc[i] != 9 and df['Semestre de Ingresso'].iloc[i] != 1:
+		x = (2020 - df['Ano de Ingresso do Curso'].iloc[i]) * 2 - (df['Etapa do Ordenamento '].iloc[i]) - 1
+		print(x)
+		series.append(int(x))
+	else:
+		series.append(" ")
+
+df['Tempo Atraso'] =  pd.Series(series)
+
+df.to_csv('Teste.csv')
